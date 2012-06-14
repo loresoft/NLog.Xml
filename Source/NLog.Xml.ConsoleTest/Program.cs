6cd4b745a8b9477413075f35afa3493270d7b615
Paul Welter <pwelter@loresoft.com>
2012-06-14 09:53:13 -0500

    - add MessageQueueTarget.cs

--- Source/NLog.Xml.ConsoleTest/Program.cs
+++ Source/NLog.Xml.ConsoleTest/Program.cs
@@ -15,13 +15,13 @@ namespace NLog.Xml.ConsoleTest
             int k = 42;
             int l = 100;
 
-            //_logger.Trace("Sample trace message, k={0}, l={1}", k, l);
-            //_logger.Debug("Sample debug message, k={0}, l={1}", k, l);
-            //_logger.Info("Sample informational message, k={0}, l={1}", k, l);
-            //_logger.Warn("Sample warning message, k={0}, l={1}", k, l);
-            //_logger.Error("Sample error message, k={0}, l={1}", k, l);
-            //_logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
-            //_logger.Log(LogLevel.Info, "Sample fatal error message, k={0}, l={1}", k, l);
+            _logger.Trace("Sample trace message, k={0}, l={1}", k, l);
+            _logger.Debug("Sample debug message, k={0}, l={1}", k, l);
+            _logger.Info("Sample informational message, k={0}, l={1}", k, l);
+            _logger.Warn("Sample warning message, k={0}, l={1}", k, l);
+            _logger.Error("Sample error message, k={0}, l={1}", k, l);
+            _logger.Fatal("Sample fatal error message, k={0}, l={1}", k, l);
+            _logger.Log(LogLevel.Info, "Sample fatal error message, k={0}, l={1}", k, l);
 
             _logger.Info()
                 .Message("Sample informational message, k={0}, l={1}", k, l)
