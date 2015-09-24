8db4e441da918612e2de2983edb13534ffb9d46b
Paul Welter <pwelter@loresoft.com>
2015-09-24 12:13:42 -0500

    update packages

--- update.cmd
+++ update.cmd
@@ -12,8 +12,6 @@ git checkout  "Source\NLog.Xml\packages.config"
 git checkout  "Source\NLog.Xml.ConsoleTest\packages.config"
 "Source\.nuget\NuGet.exe" update "Source\NLog.Xml.ConsoleTest\NLog.Xml.ConsoleTest.netfx45.csproj" -r "Source\packages"
 
-"Source\.nuget\NuGet.exe" update "Source\NLog.Xml.Test\NLog.Xml.Test.netfx40.csproj" -r "Source\packages"
-git checkout  "Source\NLog.Xml.Test\packages.config"
 "Source\.nuget\NuGet.exe" update "Source\NLog.Xml.Test\NLog.Xml.Test.netfx45.csproj" -r "Source\packages"
 
 msbuild master.proj -t:Refresh
